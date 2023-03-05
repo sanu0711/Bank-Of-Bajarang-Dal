87b5f5cb4ef75d13b855e5a0633460ea7515b6af
ABHISHEK YADAV <106998817+sanu0711@users.noreply.github.com>
2023-03-05 10:15:02 +0530

    Delete JS directory

--- JS/navigation.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', function() {
-    var nav = document.querySelector('.nav');
-    if (window.pageYOffset > 100) {
-      nav.classList.add('sticky');
-    } else {
-      nav.classList.remove('sticky');
-    }
-  });
-  
